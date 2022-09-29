cd9dd80d7e952d5a9ef04c7c61179f0d5e0b566c
Liza Leung <liza.c.leung@gmail.com>
2022-09-29 16:21:00 +0800

    updates for eb

--- app.py
+++ app.py
@@ -1,17 +1,31 @@
-# /run.py
-import os
-# from dotenv import load_dotenv, find_dotenv
+# # /run.py
+# import os
+# # from dotenv import load_dotenv, find_dotenv
 
-from src.app import create_app
+# from src.app import create_app
 
-# load_dotenv(find_dotenv())
+# # load_dotenv(find_dotenv())
 
-env_name = os.getenv('FLASK_ENV')
-print("env_name" + env_name)
+# env_name = os.getenv('FLASK_ENV')
+# print("env_name" + env_name)
 
-app = create_app(env_name)
+# app = create_app(env_name)
 
-if __name__ == '__main__':
-  port = os.getenv('5000')
-  # run app
-  app.run(host='0.0.0.0', port=port)
+# if __name__ == '__main__':
+#   port = os.getenv('5000')
+#   # run app
+#   application.debug = True
+#   app.run(host='0.0.0.0', port=port)
+
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__ == "__main__":
+       app.run()
